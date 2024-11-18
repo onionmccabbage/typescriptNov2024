8efb6365b14e02eed293340cc995e4f66ba6d28f
onionmccabbage <onionmccabbage@live.com>
2024-11-18 16:22:59 +0000

    generics

---
+++ 01_types/08_generics.ts
@@ -0,0 +1,20 @@
+// a common problem - we may not know beforehand what exactly will be the type
+
+// here we are limited to any or to very specific types
+const zeroethElement = (a:string| Array<number>)=>{ // any[]
+    return a[0]
+}
+
+// here we will allow ANY iterable type - generic type <T> makes it a generic
+const zeroethElementGeneric = <T>(a:T | Array<T>) :T=>{
+    return a[0]
+}
+
+// exercise the code
+let a = 'how does this look'
+let b = [4,3,2,1]
+let z = ['4','3','2','1']
+let y = [{},{},{},{}]
+
+console.log( zeroethElementGeneric(a), zeroethElementGeneric(b) ) // 'h' 4
+console.log( zeroethElementGeneric(y), zeroethElementGeneric(z) ) //
