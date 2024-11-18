5f766a0713542c37695485b98c95345c93db9e81
onionmccabbage <onionmccabbage@live.com>
2024-11-18 15:29:16 +0000

    tuple

---
+++ 01_types/03_tuple.ts
@@ -0,0 +1,8 @@
+// ts lets us type-safe the members of a JS array
+// a ts tuple lets us specify the order of the types of the members of the array
+
+let poem:[number, boolean, string] // a tuple
+// poem = ['wandered', 'lonely', 'cloud']
+poem = [1, true, 'love']
+
+console.log(poem)
