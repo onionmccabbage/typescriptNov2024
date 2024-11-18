21d48a23c79fba7e8ff853e982225f26ca449216
onionmccabbage <onionmccabbage@live.com>
2024-11-18 12:27:19 +0000

    Photo implementation

--- 01_types/02_classes.ts
+++ 01_types/02_classes.ts
@@ -1,14 +1,32 @@
 // JS has no idea of interface, but can do classes
 // NB  ...but classes are just OBJECTS in JS
 
-class Snap {
+interface Photo {
+    //some properties
+    albumId:number
+    id:number
     title:string
     url:string
+    thumbnailUrl:string
+    // some methods
+    // any property or method may be optional  using '?'
+    prettyPrint?: ()=>{} // no implementation
+}
+
+class Snap implements Photo {
+    albumId:number
+    id:number
+    title!:string // ! means we guarantee it will be not-null
+    url:string
+    thumbnailUrl:string
     // NB remember at RUNTIME we may still pass in a non-string and JS will be fine
     constructor(title:string, url:string) { // only (zero or) one constructor in JS
         this.title = title
         this.url   = url
     }
+    prettyPrint = ():string => {
+        return `This snap is ${this.title} url ${this.url}`
+    };
 }
 
 // make instances
