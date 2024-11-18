3e22c27a209e40d30fd9e4d8645a1960c9827666
onionmccabbage <onionmccabbage@live.com>
2024-11-18 15:34:04 +0000

    enum

---
+++ 01_types/04_enum.ts
@@ -0,0 +1,9 @@
+enum BoxSize { Small, Medium} // these are 0, 1 in the enum
+
+enum BoxSize { Large=2, XLarge} // carry on the enumeration
+
+let c = BoxSize.Medium
+
+c++ // (not the language) increment c by 1
+
+console.log(c, BoxSize.Small)
