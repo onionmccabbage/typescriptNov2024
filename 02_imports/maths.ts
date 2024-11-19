bf0e9a8c4b6964502fafab275e937b94f2102281
onionmccabbage <onionmccabbage@live.com>
2024-11-19 09:58:27 +0000

    imports

---
+++ 02_imports/maths.ts
@@ -0,0 +1,8 @@
+var pi         = 3.14
+let sqrRootTwo = 1.41
+const phi      = 1.61 // immutable
+
+class Arbitrary {
+    // nothing to see here
+}
+
---
+++ 02_imports/util.ts
@@ -0,0 +1,9 @@
+interface Penguin {
+    breed:string
+    yearOfBirth:number
+}
+
+type Dog = {
+    breed:string[]
+    yearOfBirth:number
+}
