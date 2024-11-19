6db163ef71c714c3e11ab6e97e1b6a6fb2415ec6
onionmccabbage <onionmccabbage@live.com>
2024-11-19 16:16:24 +0000

    generator

---
+++ 03_ES_features/04_generators.ts
@@ -0,0 +1,22 @@
+// generators have been available since ES6 (2015)
+// they are VERY efficient
+
+// we MUST use 'function*' ( not ()=>{} )
+function* fib(){
+    // generate the next member in the fibbonacci sequence
+    // i.e. 1, 2, 3, 5, 8, 13, 21, 34 ...
+    let first=1, second=2
+    for(;;){ // endless loop
+        let sum = first+second
+        yield sum // we yield (not return) so the function* does not end
+        first = second
+        second = sum
+    }
+}
+// exercise the code
+const prettyPrint = ()=>{}
+const g = fib() // an instance of our generator
+console.log( g.next() ) // 3
+console.log( g.next() ) // 5
+console.log( g.next() ) // 8
+console.log( g.next().value ) // 13
