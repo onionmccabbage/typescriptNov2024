5e29fffc09837ca088b1e092f709d037af61b7f7
onionmccabbage <onionmccabbage@live.com>
2024-11-19 13:57:02 +0000

    automation and async-await with fetch

--- 04_automation/dist/app.js
+++ 04_automation/dist/app.js
@@ -1,11 +1,18 @@
+// NB in the generated JS we must remember to import from promises.js
 import { fetchPhotos } from "./promises.js";
+// we need to grab a handle to bits of the HTML DOM
+const c = document.getElementById('content');
+const i = document.getElementById('thumbnail');
 // make a call to our function
 // promises are 'then-able'
-console.log('nearly lunch')
 fetchPhotos() // this is async will return a Promise
     // when fetchPhotos returns something, only then do we ...
     .then((d) => {
-        console.log(d[0]);
+    console.log(d[0].thumbnailUrl);
+    // we can use our DOM id handles to inject into hte web page
+    c.innerHTML = d[3]['title']; // grab part of our data
+    i.setAttribute('src', d[3]['thumbnailUrl']);
+    i.setAttribute('alt', d[3]['title']); // always consider the 'alt' for accesibility
 })
     .catch((err) => {
     console.log(err);
--- 04_automation/src/app.ts
+++ 04_automation/src/app.ts
@@ -1,11 +1,20 @@
 // NB in the generated JS we must remember to import from promises.js
 import { fetchPhotos, Photo } from "./promises";
+
+// we need to grab a handle to bits of the HTML DOM
+const c = document.getElementById('content') // CAREFUL - this will ONLY work in a browser
+const i = document.getElementById('thumbnail')
+
 // make a call to our function
 // promises are 'then-able'
 fetchPhotos() // this is async will return a Promise
 // when fetchPhotos returns something, only then do we ...
     .then( (d:Photo[])=>{ // or Array<Photo>
         console.log(d[0].thumbnailUrl)
+        // we can use our DOM id handles to inject into hte web page
+        c.innerHTML = d[4]['title'] // grab part of our data
+        i.setAttribute('src', d[4]['thumbnailUrl'])
+        i.setAttribute('alt', d[4]['title']) // always consider the 'alt' for accesibility
     } )
     .catch( (err)=>{
         console.log(err)
