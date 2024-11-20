ee0790b5e282d432589a79e5853e813fdbd075e9
onionmccabbage <onionmccabbage@live.com>
2024-11-20 12:02:20 +0000

    approaching Observables

--- 07_observables/src/app.ts
+++ 07_observables/src/app.ts
@@ -11,7 +11,7 @@ const testData:string[] = [
 
 // check for a match
 //               testData      whatever the useer typed
-const suggest = (arr:string[], query:string)=>{
+const suggest = (arr:string[], query:string):string[]=>{
     return arr.filter( (item)=>{
         return query.length>0 && item.startsWith(query)
     } )
@@ -21,3 +21,17 @@ const suggest = (arr:string[], query:string)=>{
 const cleanUpUtil = (container:HTMLElement)=>{
     container.innerHTML = ''
 }
+
+// here is the function for append any search suggestions into the web page
+const appendResults = (container:HTMLElement, results:string[])=>{
+    for (const result of results) {
+        // we need a fresh <li> for each suggestion
+        const li = document.createElement('li')
+        const innerText = document.createTextNode(result)
+        li.appendChild(innerText)
+        container.appendChild(li)
+    }
+}
+// we need to grab some bits of the DOM
+const searchBox:HTMLElement = document.querySelector('#search') // or .getElementById('search)
+const results:HTMLElement   = document.querySelector('#results')
